71a917d0d8fe535c9f7c859cd5133347aa770efb
AleksaDesign35 <hi@kremicalex.com>
2024-12-09 22:27:51 +0100

    "First Lession"

---
+++ 1-CAS/index.js
@@ -0,0 +1,17 @@
+// console.log(__dirname);
+// console.log(__filename);
+
+let user = require("./moduls/component");
+
+console.log(user("developer"));
+
+
+
+// let os = require("os");
+// let cpus = os.cpus()[0];
+
+// if(cpus < 3000){
+//    console.log("Procesor ti je sporedan");
+// } else{
+//    console.log("Procesor ti je dobar");
+// }
